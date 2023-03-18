a2825af374082b5cdb74b0f8ce3349b79d31d52e
JT Archie <jtarchie@gmail.com>
2023-03-18 12:24:02 -0600

    use json decoding from the library

--- sdk/stats.go
+++ sdk/stats.go
@@ -1,7 +1,6 @@
 package sdk
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -13,23 +12,18 @@ type StatsPayload struct {
 }
 
 func (c *Client) Stats() (*StatsPayload, error) {
+	payload := &StatsPayload{}
+
 	client := c.client
 
-	response, err := client.R().Get(fmt.Sprintf("%s/api/stats", c.endpoint))
+	response, err := client.R().
+		SetSuccessResult(payload).
+		Get(fmt.Sprintf("%s/api/stats", c.endpoint))
 	if err != nil {
 		return nil, fmt.Errorf("could not GET /api/stats: %w", err)
 	}
 
 	if response.StatusCode == http.StatusOK {
-		payload := &StatsPayload{}
-		err = json.NewDecoder(response.Body).Decode(payload)
-
-		defer response.Body.Close()
-
-		if err != nil {
-			return nil, fmt.Errorf("could not parse /api/status: %w", err)
-		}
-
 		return payload, nil
 	}
 
